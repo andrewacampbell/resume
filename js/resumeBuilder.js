bb9fd1f7be21e0bad0d155a118639e2819b0578b
Andrew <legero_cam@yahoo.com>
2016-03-18 08:29:41 -0500

    add header of resume

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,2 +1,49 @@
-$("#main").append("name");
-  
+var name = "Andrew Campbell";
+var role = "Software Engineer";
+
+var jobtitle = HTMLheaderRole.replace("%data%", role);
+$("#header").prepend(jobtitle);
+
+var fName = HTMLheaderName.replace("%data%", name);
+$("#header").prepend(fName);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
